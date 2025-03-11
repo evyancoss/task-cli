0ecf5cfc4d726d2609624bae841948707786108c
Evyan Coss <brandoncoss01@gmail.com>
2025-03-11 01:23:57 -0600

    End proyect

--- main.py
+++ main.py
@@ -11,15 +11,12 @@ def update(uid: int, description: str):
     print("Update task: {}".format(uid))
 
 def delete(uid: int):
-    
     print("Task: {}".format(uid))
     return task.delete(uid)
 
-def mark_in_progress(uid: int):
-    print('')
-
-def mark_done(uid: int):
-    print('')
+def status(uid: int, status: str):
+    return task.change_status(uid,status)
+    
 
 def list_tasks(status=None):
     print("List tasks")
@@ -35,7 +32,7 @@ if __name__ == "__main__":
 
     # Add task command
     add_parser = subparsers.add_parser("add", help="Add a task")
-    add_parser.add_argument("description", type=str, help="Task description")
+    add_parser.add_argument("--description", type=str, help="Task description")
 
     # List tasks command
     list_parser = subparsers.add_parser("list", help="List all tasks")
@@ -50,16 +47,30 @@ if __name__ == "__main__":
     delete_parser = subparsers.add_parser("delete", help="Delete a task")
     delete_parser.add_argument("uid", type=int, help="Task ID")
 
+    mark_in_progress = subparsers.add_parser("mark-in-progress", help="Change task to mark_in_progress")
+    mark_in_progress.add_argument("uid", type=int, help="Task ID")
+    
+    mark_done = subparsers.add_parser("mark-done", help="Change task to mark-done")
+    mark_done.add_argument("uid", type=int, help="Task ID")
+
     # Parse command-line arguments
     args = parser.parse_args()
-    print(args)
-
-    if args.command == 'list':
+    
+    if args.command == "list":
         tasks = list_tasks(args.status)
         print(tasks)
-    elif args.command == 'add':
+
+    elif args.command == "add":
         add(args.description)
-    elif args.command == 'update':
+
+    elif args.command == "update":
         update(args.uid,args.description)
-    elif args.command == 'delete':
+
+    elif args.command == "delete":
         delete(args.uid)
+
+    elif args.command == "mark-in-progress":
+        status(args.uid,"mark-in-progress")
+
+    elif args.command == "mark-done":
+        status(args.uid,"mark-done")
--- models/task.py
+++ models/task.py
@@ -2,18 +2,26 @@ from datetime import datetime
 import json, os, random
 
 def file_tasks():
-    if not os.path.exists('src/tasks.json'):
-        with open('src/tasks.json', 'w') as file:
+    if not os.path.exists("src/tasks.json"):
+        with open("src/tasks.json", "w") as file:
             json.dump([], file)
 
-    with open('src/tasks.json', 'r') as file:
+    with open("src/tasks.json", "r") as file:
         return json.load(file)
+    
+def save_tasks(change: bool, type: str, list_tasks):
+    if change:
+        with open("src/tasks.json", "w") as file:
+            json.dump(list_tasks, file, indent=4)
+        return "Was {} success".format(type)
+    else:
+        return "Task no found"
     
 
 def list(status = None):
         tasks = file_tasks()
         if status:
-            tasks = [task for task in tasks if task['status'] == status]
+            tasks = [task for task in tasks if task["status"] == status]
         else:
             tasks = [task for task in tasks] 
         return tasks
@@ -23,49 +31,51 @@ def add(description: str):
         uid = random.randint(1, 1000)
 
         new_task= {
-            'uid': uid,
-            'description':description,
-            'status':'todo',
-            'createdAt': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'updatedAt':''
+            "uid": uid,
+            "description":description,
+            "status":"todo",
+            "createdAt": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "updatedAt":""
         }
         
         tasks.append(new_task)
 
-        with open('src/tasks.json', 'w') as file:
+        with open("src/tasks.json", "w") as file:
             json.dump(tasks, file, indent=4)
 
-        return new_task['uid']
+        return new_task["uid"]
     
 def update(uid:int, description: str):
     list_tasks = file_tasks()
     updated = False
     for task in list_tasks:
-        if task['uid'] == uid:
+        if task["uid"] == uid:
             updated = True
-            task['description'] = description
-            task['updatedAt'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            task["description"] = description
+            task["updatedAt"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             break
     
-    if updated:
-        with open('src/tasks.json', 'w') as file:
-            json.dump(list_tasks, file, indent=4)
-        return 'Task update success'
-    else:
-        return 'Task no found'
+    return save_tasks(updated, "update", list_tasks) 
+
 
 def delete(uid:int):
     list_tasks = file_tasks()
     deleted = False
     for task in list_tasks:
-        if task['uid'] == uid:
+        if task["uid"] == uid:
             deleted = True
             list_tasks.remove(task)
             break
     
-    if deleted:
-        with open('src/tasks.json', 'w') as file:
-            json.dump(list_tasks, file, indent=4)
-        return 'Was deleted success'
-    else:
-        return 'Task no found'
+    return save_tasks(deleted, "deleted", list_tasks)
+    
+def change_status(uid: int, status: str):
+    list_tasks = file_tasks()
+    changed = False
+    for task in list_tasks:
+        if task["uid"] == uid:
+            changed = True
+            task["status"] = status
+            break
+    
+    return save_tasks(changed, "changed", list_tasks)
